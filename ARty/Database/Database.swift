3f0431c663a55b53ae2d125c2d1733e14f8ba492
Quan Vo <qvo1987@gmail.com>
2018-05-31 22:24:22 -0500

    add firestore, some user helper functions

---
+++ ARty/Database/Database.swift
@@ -0,0 +1,60 @@
+import Firebase
+
+struct Database {
+    private static let db: Firestore = {
+        let db = Firestore.firestore()
+        db.settings.areTimestampsInSnapshotsEnabled = true
+        return db
+    }()
+
+    private static let usersCollection = db.collection("users")
+
+    private init() {}
+
+    static func setUser(_ user: User, completion: @escaping (Error?) -> Void) {
+        usersCollection.document(user.uid).setData(user.dictionary) { error in
+            completion(error)
+        }
+    }
+
+    static func setARty(_ arty: ARty, completion: @escaping (Error?) -> Void) {
+        usersCollection.document(arty.uid).updateData([
+            "model": arty.model,
+            "passiveAnimation": arty.passiveAnimation,
+            "pokeAnimation": arty.pokeAnimation
+        ]) { error in
+            completion(error)
+        }
+    }
+
+    static func setPassiveAnimation(_ passiveAnimation: String,
+                                    for uid: String,
+                                    completion: @escaping (Error?) -> Void) {
+        usersCollection.document(uid).updateData([
+            "passiveAnimation" : passiveAnimation
+        ]) { error in
+            completion(error)
+        }
+    }
+
+    static func setPokeAnimation(_ pokeAnimation: String,
+                                 for uid: String,
+                                 completion: @escaping (Error?) -> Void) {
+        usersCollection.document(uid).updateData([
+            "pokeAnimaton" : pokeAnimation
+        ]) { error in
+            completion(error)
+        }
+    }
+
+    static func setLocation(latitude: Double,
+                            longitude: Double,
+                            uid: String,
+                            completion: @escaping (Error?) -> Void) {
+        usersCollection.document(uid).updateData([
+            "location" : GeoPoint(latitude: latitude, longitude: longitude)
+        ]) { error in
+            completion(error)
+        }
+    }
+}
--- ARty/Model/User.swift
+++ ARty/Model/User.swift
@@ -1,8 +1,51 @@
+import FirebaseFirestore
+
 struct User {
     let uid: String
     let model: String
     let passiveAnimation: String
     let pokeAnimation: String
-    let x: Double
-    let z: Double
+    let latitude: Double
+    let longitude: Double
+
+    init(uid: String,
+         model: String,
+         passiveAnimation: String,
+         pokeAnimation: String,
+         latitude: Double,
+         longitude: Double) {
+        self.uid = uid
+        self.model = model
+        self.passiveAnimation = passiveAnimation
+        self.pokeAnimation = pokeAnimation
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+
+    init(_ data: [String: Any]?) throws {
+        guard let uid = data?["uid"] as? String,
+            let model = data?["model"] as? String,
+            let passiveAnimation = data?["passiveAnimation"] as? String,
+            let pokeAnimation = data?["pokeAnimation"] as? String,
+            let location = data?["location"] as? GeoPoint else {
+                throw ARtyError.invalidDataFromServer(data)
+        }
+        self.uid = uid
+        self.model = model
+        self.passiveAnimation = passiveAnimation
+        self.pokeAnimation = pokeAnimation
+        self.latitude = location.latitude
+        self.longitude = location.longitude
+    }
+
+    var dictionary: [String: Any] {
+        return [
+            "uid": uid,
+            "model": model,
+            "passiveAnimation": passiveAnimation,
+            "pokeAnimation": pokeAnimation,
+            "latitude": latitude,
+            "longitude": longitude
+        ]
+    }
 }
